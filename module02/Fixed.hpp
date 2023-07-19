4e4983df241abfbb91c8c67d3b7eebce3ab6494a
Achraf Dardour <adardour@e3r7p17.1337.ma>
2023-07-19 21:50:31 +0100

    dd

---
+++ module02/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const Fixed &copy);
+		~Fixed();
+		int getRawBits(void) const;
+		void	setRawBits(int const raw);
+		
+	private:
+	 	int	fixed_point;
+		static const int fractional = 8;
+		
+};
+
+#endif
